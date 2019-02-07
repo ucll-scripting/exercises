2fc29d2b72740eb3764d6d0024447dd30b5b331a
Frederic Vogels <frederic.vogels@ucll.be>
2019-02-07 13:42:13 +0100

    shell-tools/wc

--- shell-tools/wc/wc.py
+++ shell-tools/wc/wc.py
@@ -4,45 +4,93 @@ import re
 import os
 
 
-def _line_count(filename):
+def line_count(filename):
     with open(filename, 'r') as file:
         return len(file.readlines())
 
-def _word_count(filename):
+
+def word_count(filename):
     with open(filename, 'r') as file:
         return len(re.split(r'\s+', file.read()))
 
-def _byte_count(filename):
+
+def byte_count(filename):
     return os.path.getsize(filename)
 
-def _char_count(filename):
+
+def char_count(filename):
     with open(filename, 'r') as file:
         return len(file.read())
 
 
-def process_command_line_arguments():
+def print_table(table):
+    def column(i):
+        return (row[i] for row in table)
+
+    def column_width(i):
+        return max( len(cell) for cell in column(i) )
+
+    n_columns = len(table[0])
+    column_widths = [ column_width(i) for i in range(n_columns) ]
+
+    def format_row(row):
+        def format_cell(i):
+            return row[i].rjust(column_widths[i])
+
+        return " ".join( format_cell(i) for i in range(len(row)) )
+
+    for row in table:
+        print(format_row(row))
+
+
+def create_parser():
     parser = argparse.ArgumentParser(prog='wc')
+
     parser.add_argument('files', help='print line count', nargs='+')
-    parser.add_argument('-l', '--lines', help='print line count', dest='counters', action='append_const', const=_line_count)
-    parser.add_argument('-c', '--bytes', help='print byte counts', dest='counters', action='append_const', const=_byte_count)
-    parser.add_argument('-m', '--chars', help='print character counts', dest='counters', action='append_const', const=_char_count)
-    parser.add_argument('-w', '--words', help='print word counts', dest='counters', action='append_const', const=_word_count)
+    parser.add_argument('-l', '--lines', help='print line count', dest='counters', action='append_const', const=line_count)
+    parser.add_argument('-c', '--bytes', help='print byte counts', dest='counters', action='append_const', const=byte_count)
+    parser.add_argument('-m', '--chars', help='print character counts', dest='counters', action='append_const', const=char_count)
+    parser.add_argument('-w', '--words', help='print word counts', dest='counters', action='append_const', const=word_count)
 
-    args = parser.parse_args()
+    return parser
 
-    counters = args.counters or [ _line_count, _word_count, _byte_count ]
+
+def collect_data(files, counters):
+    def process_file(file):
+        return (*( counter(file) for counter in counters ), file)
+
+    return [ process_file(f) for f in files ]
+
+
+def add_total_row(table):
+    if len(table) > 1:
+        n_columns = len(table[0])
+        sums = ( sum(t[i] for t in table) for i in range(n_columns - 1) )
+        table.append(tuple( [ *sums, 'total' ]))
+
+
+def convert_to_strings(table):
+    return [ tuple(str(x) for x in row) for row in table ]
+
+
+def main():
+    args = create_parser().parse_args()
+
+    # If no counters were specified, use defaults
+    counters = args.counters or [ line_count, word_count, byte_count ]
     files = args.files
 
-    data = [ (*[ c(f) for c in counters ], f) for f in files ]
+    # Collect counts for each file
+    table = collect_data(files, counters)
 
-    if len(data) > 1:
-        data.append(tuple((*tuple( sum(t[i] for t in data) for i in range(len(counters)) ), 'total')))
+    # Add total row if necessary
+    add_total_row(table)
 
-    string_data = [ tuple( str(x) for x in row) for row in data ]
-    column_widths = [ max( len(row[i]) for row in string_data ) for i in range(len(string_data[0])) ]
+    # Convert all data to strings
+    string_table = convert_to_strings(table)
 
-    for row in string_data:
-        print(" ".join( row[i].rjust(column_widths[i]) for i in range(len(row)) ))
+    # Print table
+    print_table(string_table)
 
 
-process_command_line_arguments()
+main()
