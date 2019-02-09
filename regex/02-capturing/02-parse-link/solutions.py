4ff03010ecdcad2c2497ea491a7b75c094e9769d
Frederic Vogels <frederic.vogels@ucll.be>
2019-02-09 12:38:06 +0100

    regex/02-capturing/02-parse-link

--- regex/02-capturing/02-parse-link/solutions.py
+++ regex/02-capturing/02-parse-link/solutions.py
@@ -3,10 +3,8 @@ import re
 
 def parse_link(string):
     match = re.fullmatch(r'<a href="(.*)">(.*)</a>', string)
-
     if match:
         url, caption = match.groups()
-
         return (caption, url)
     else:
         return None
