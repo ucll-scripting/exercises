c0b033b6fbb7c55e7d2e6a3d1ad4a5edc8e5f46d
Frederic Vogels <frederic.vogels@ucll.be>
2019-03-05 23:00:41 +0100

    shell-tools/tail

---
+++ shell-tools/tail/tail.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import argparse
+import sys
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(prog='tail')
+    parser.add_argument('file', help='file', nargs='?')
+    parser.add_argument('-n', '--lines', help='number of lines to print', action='store', default=5, type=int)
+    return parser
+
+
+def print_tail(stream, n):
+    lines = []
+
+    for line in stream:
+        lines.append(line)
+        lines = lines[-n:]
+
+    for line in lines:
+        print(line, end="")
+
+
+args = create_parser().parse_args()
+
+if args.file:
+    with open(args.file, 'r') as file:
+        print_tail(file, args.lines)
+else:
+    print_tail(sys.stdin, args.lines)
