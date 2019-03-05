f877789a588d12ebe54f942b18ef39eeef6de547
Frederic Vogels <frederic.vogels@ucll.be>
2019-03-05 23:00:38 +0100

    shell-tools/head

--- shell-tools/head/head.py
+++ shell-tools/head/head.py
@@ -6,12 +6,20 @@ import sys
 
 def create_parser():
     parser = argparse.ArgumentParser(prog='head')
-    parser.add_argument('file', help='file')
+    parser.add_argument('file', help='file', nargs='?')
     parser.add_argument('-n', '--lines', help='number of lines to print', action='store', default=5, type=int)
     return parser
 
+
+def print_head(stream, n):
+    for _ in range(n):
+        print(stream.readline(), end='')
+
+
 args = create_parser().parse_args()
 
-with open(args.file, 'r') as file:
-    for _ in range(args.lines):
-        print(file.readline(), end='')
+if args.file:
+    with open(args.file, 'r') as file:
+        print_head(file, args.lines)
+else:
+    print_head(sys.stdin, args.lines)
