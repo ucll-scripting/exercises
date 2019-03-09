6a4c207255b82a95b0fd2518f0bab2fb859b0b5e
Frederic Vogels <frederic.vogels@ucll.be>
2019-03-09 12:17:20 +0100

    04-scripting/02-io/04-download

--- 04-scripting/02-io/04-download/solution.py
+++ 04-scripting/02-io/04-download/solution.py
@@ -2,5 +2,8 @@ import urllib.request
 import sys
 
 
-with urllib.request.urlopen(sys.argv[1]) as input:
-    print(input.read())
+word = sys.argv[1]
+
+with urllib.request.urlopen(f"https://www.dictionary.com/browse/{word}") as input:
+    contents = input.read()
+    print(contents)
