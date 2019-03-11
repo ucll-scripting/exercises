9b7bffbadea3e7bee970b899a5355b00faf532cb
Frederic Vogels <frederic.vogels@ucll.be>
2019-03-11 11:28:12 +0100

    04-scripting/03-argparse/03-find

---
+++ 04-scripting/03-argparse/03-find/solution.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+import argparse
+from pathlib import Path
+import os
+
+
+def minimum_size(size):
+    def check(filename):
+        return os.path.getsize(filename) >= size
+
+
+
+parser = argparse.ArgumentParser(prog='find')
+parser.add_argument('path', default='.')
+parser.add_argument('--minimum-size', dest='minimum_size', default=0, type=int)
+parser.add_argument('--maximum-size', dest='maximum_size', default=float('inf'), type=int)
+parser.add_argument('--no-directories', dest='no_directories', default=False, action='store_true')
+parser.add_argument('--no-files', dest='no_files', default=False, action='store_true')
+parser.add_argument('--extension', dest='extension')
+
+args = parser.parse_args()
+
+
+path = Path(args.path)
+for entry in path.glob('**/*'):
+    if not os.path.getsize(entry) >= args.minimum_size:
+        continue
+
+    if not os.path.getsize(entry) <= args.maximum_size:
+        continue
+
+    if args.no_directories and os.path.isdir(entry):
+        continue
+
+    if args.no_files and os.path.isfile(entry):
+        continue
+
+    if args.extension and not entry.suffix == args.extension:
+        continue
+
+    print(entry)
