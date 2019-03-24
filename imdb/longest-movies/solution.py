739abaece5fdef25ad247f93671ad44b08ce96d7
Frederic Vogels <frederic.vogels@ucll.be>
2019-03-24 15:01:50 +0100

    imdb/longest-movies

--- imdb/longest-movies/solution.py
+++ imdb/longest-movies/solution.py
@@ -2,4 +2,4 @@ from pandas import read_csv
 
 
 title_basics = read_csv('../title-basics.tsv', sep='\t', low_memory=False)
-print(title_basics[title_basics['titleType'] == 'movie'][title_basics['runtimeMinutes'] != r'\N'].astype( { 'runtimeMinutes': 'int32' } ).sort_values(['runtimeMinutes'], ascending=False).loc[:,['primaryTitle', 'runtimeMinutes']].head(100).to_string(index=False))
+print(title_basics[title_basics.titleType == 'movie'][title_basics.runtimeMinutes != r'\N'].astype( { 'runtimeMinutes': 'int32' } ).sort_values(['runtimeMinutes'], ascending=False).loc[:,['primaryTitle', 'runtimeMinutes']].head(100).to_string(index=False))
