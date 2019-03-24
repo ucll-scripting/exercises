757d21a85ff0c0930bd07d79059199a1aa9424c2
Frederic Vogels <frederic.vogels@ucll.be>
2019-03-24 11:47:05 +0100

    imdb

--- imdb/solution.py
+++ imdb/solution.py
@@ -1,27 +1,58 @@
-from contextlib import contextmanager
+from tabulate import tabulate
+from sys import stderr
+import pandas
 import csv
+import re
 
 
+def log(string):
+    print(string, file=stderr, flush=True)
 
-@contextmanager
-def open_tsv(filename):
+def read(filename):
+    log(f'Reading {filename}...')
+
     with open(filename, encoding='utf-8') as file:
-        yield csv.DictReader(file, dialect='excel-tab')
+        return list(csv.DictReader(file, dialect='excel-tab'))
+
+title_basic = read('title-basics.tsv')
 
 
-def find_longest():
-    with open_tsv('title-basics.tsv') as tsv:
-        return max(int(row['runtimeMinutes']) for row in tsv if str.isnumeric(row['runtimeMinutes']))
+def find_top_longest_movies():
+    log('Finding longest movies')
+    global title_basic
+    movie_runtime_pairs = ( (row['primaryTitle'], int(row['runtimeMinutes'])) for row in title_basic if  row['titleType'] == 'movie' and str.isnumeric(row['runtimeMinutes']) )
+    sorted_by_runtime = sorted(movie_runtime_pairs, key=lambda pair: pair[1], reverse=True)
+
+    print('Top 100 longest movies')
+    print(tabulate(sorted_by_runtime[:100], headers=['Title', 'Runtime']))
 
 
-def count_type(type):
-    with open_tsv('title-basics.tsv') as tsv:
-        return sum(1 for row in tsv if row['titleType'] == type)
+def find_top_longest_movie_titles():
+    log('Finding longest movie titles')
+    global title_basic
+    titles = ( row['primaryTitle'] for row in title_basic if row['titleType'] == 'movie' )
+    sorted_by_title_length = sorted(titles, key=len, reverse=True)
+
+    print('Top 100 longest titles')
+    top = sorted_by_title_length[:100]
+    print(tabulate(((title, len(title)) for title in top), headers=['Title', 'Title Length']))
 
 
-def find_people_with_name(name):
-    with open_tsv('name-basics.tsv') as tsv:
-        return [ row for row in tsv if row['primaryName'] == name ]
+def counts_by_type():
+    log('Computing counts by types')
+    table = {}
+    for row in title_basic:
+        type = row['titleType']
+        old_count = table.setdefault(type, 0)
+        new_count = old_count + 1
+        table[type] = new_count
+    print('Counts by type')
+    print(tabulate( sorted(table.items(), key=lambda x: x[1], reverse=True), headers=['Type', 'Count']))
 
 
-sergio = find_people_with_name('Sergio Leone')[0]
+
+# for f in [ find_top_longest_movies,
+#            find_top_longest_movie_titles,
+#            counts_by_type ]:
+#     f()
+#     print('')
