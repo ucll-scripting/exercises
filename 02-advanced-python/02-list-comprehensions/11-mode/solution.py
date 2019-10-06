bb688210bb25bca3bd61aac3a43346306abe6a4e
stuke82 <47459076+stuke82@users.noreply.github.com>
2019-10-06 11:45:32 +0200

    Update solution.py
    Corrected & promoted the lambda solution

--- 02-advanced-python/02-list-comprehensions/11-mode/solution.py
+++ 02-advanced-python/02-list-comprehensions/11-mode/solution.py
@@ -8,16 +8,14 @@ def frequencies(ns):
 
     return result
 
+# Using a lambda
+def mode2(ns):
+    return max(frequencies(ns).items(), key=lambda pair: pair[1])[0]
 
+# Alternative
 def second(pair):
     return pair[1]
 
-
 def mode(ns):
     fs = frequencies(ns)
-
     return max(fs.items(), key=second)[0]
-
-# Alternative using lambda's
-def mode2(ns):
-    return max(frequencies(ns), key=lambda pair: pair[1])[0]
