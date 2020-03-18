8293f7c9b720ef63fce205565c4a28394931f7fe
Frederic Vogels <frederic.vogels@ucll.be>
2020-03-18 11:45:33 +0100

    02-advanced-python/02-list-comprehensions/10-morse

--- 02-advanced-python/02-list-comprehensions/10-morse/solution.py
+++ 02-advanced-python/02-list-comprehensions/10-morse/solution.py
@@ -30,7 +30,7 @@ from_morse_table = dict( (y, x) for x, y in to_morse_table.items() )
 
 
 def to_morse(string):
-    return '   '.join(' '.join( to_morse_table[char] for char in word ) for word in string.upper().split(' '))
+    return '   '.join( [ ' '.join( [ to_morse_table[char] for char in word ] ) for word in string.upper().split(' ') ] )
 
 def from_morse(string):
-    return ' '.join( ''.join( from_morse_table[char] for char in word.split(' ') ) for word in string.split('   ') )
+    return ' '.join( [ ''.join( [ from_morse_table[char] for char in word.split(' ') ] ) for word in string.split('   ') ] )
