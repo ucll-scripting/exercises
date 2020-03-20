703593a5855d2dd477644aa12aae032491646345
Frederic Vogels <frederic.vogels@ucll.be>
2020-03-20 21:38:27 +0100

    fixed tests in remove trailing whitespace

--- 03-regex/03-substituting/01-remove-trailing-whitespace/tests.py
+++ 03-regex/03-substituting/01-remove-trailing-whitespace/tests.py
@@ -8,23 +8,23 @@ with reference_file('solution.py'):
     with reference_based_test('remove_trailing_whitespace') as testcase:
         testcase('''
         fdjfkld jfjs fjdslfk
-        ''')
+        '''.strip())
 
         testcase('''
         fdjfkld jfjs fjdslfk\x20
-        ''')
+        '''.strip())
 
         testcase('''
         fdjfkld jfjs fjdslfk\x20\x20
-        ''')
+        '''.strip())
 
         testcase('''
         fdjfkld jfjs fjdslfk\t\t
-        ''')
+        '''.strip())
 
         testcase('''
         fdf qqip saofp k\x20\t\x20
         fjdklfj f sfjslkf\x20\x20\x20\x20\x20\x20
         fdjfkldjf\x20\x20
-        ''')
+        '''.strip())
 
