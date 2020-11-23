eaf0de60d0f29d8111b49771ba5359884397a964
stuke82 <47459076+stuke82@users.noreply.github.com>
2020-11-23 16:56:43 +0100

    Update solution.py
    Revert to original solution

--- 03-regex/02-capturing/01-parse-time/solution.py
+++ 03-regex/02-capturing/01-parse-time/solution.py
@@ -1,9 +1,7 @@
 import re
 
-
 def parse_time(string):
     match = re.fullmatch(r'(\d{2}):(\d{2}):(\d{2})(?:\.(\d{3}))?', string)
-    #match = re.fullmatch(r'([01][0-9]|[2][0-3])\:([0-5][0-9])\:([0-5][0-9])(\.[0-9]{3})?', string) is more correct, limiting the minutes and seconds to 59
 
     if match:
         return tuple( [ int(x) for x in match.groups('000') ] )
