431cc3c18b508268c1fd267a6d0de1271fa96e20
Frederic Vogels <frederic.vogels@ucll.be>
2021-02-05 00:27:38 +0100

    05-modules/zipfile/crack

---
+++ 05-modules/zipfile/crack/solution.py
@@ -0,0 +1,25 @@
+from zipfile import ZipFile, BadZipFile
+from itertools import product
+import string
+import sys
+
+
+filename = sys.argv[1]
+alphabet = string.ascii_lowercase
+
+with ZipFile(filename) as zip:
+    for password in ("".join(combination) for combination in product(alphabet, repeat=4)):
+        print(f'Trying #{password}')
+        pwd = bytes(password, 'ascii')
+        try:
+            bs = zip.read('file.txt', pwd=pwd)
+            print(f"Found password! {password}")
+            string = bs.decode('utf-8')
+            print(string)
+            sys.exit(0)
+        except RuntimeError as e:
+            pass
+        except BadZipFile:
+            pass
+
+print('Failed to find password')
