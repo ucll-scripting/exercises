0a2787234137b6c90606d2fd0bfec7a7fb36637f
Frederic Vogels <frederic.vogels@ucll.be>
2021-03-02 21:31:03 +0100

    05-modules/data-formats/imdb/longest-movies

--- 05-modules/data-formats/imdb/longest-movies/solution.py
+++ 05-modules/data-formats/imdb/longest-movies/solution.py
@@ -1,5 +1,29 @@
-from pandas import read_csv
+import csv
+import sys
 
 
-title_basics = read_csv('../title-basics.tsv', sep='\t', low_memory=False)
-print(title_basics[title_basics.titleType == 'movie'][title_basics.runtimeMinutes != r'\N'].astype( { 'runtimeMinutes': 'int32' } ).sort_values(['runtimeMinutes'], ascending=False).loc[:,['primaryTitle', 'runtimeMinutes']].head(100).to_string(index=False))
+def second(xs):
+    return xs[1]
+
+def runtime(row):
+    result = row['runtimeMinutes']
+    if result == r"\N":
+        return 0
+    else:
+        try:
+            return int(result)
+        except:
+            print(row)
+            raise
+
+
+with open("../title-basics.tsv", encoding='utf-8') as file:
+    reader = csv.DictReader(file, delimiter='\t', quoting=csv.QUOTE_NONE)
+    results = [ (row['originalTitle'], runtime(row)) for row in reader if row['runtimeMinutes'] != r'\N' ]
+
+
+results.sort(key=second, reverse=True)
+top_100 = results[:100]
+
+for title, runtime in top_100:
+    print(f"{str(runtime).rjust(5)} {title}")
