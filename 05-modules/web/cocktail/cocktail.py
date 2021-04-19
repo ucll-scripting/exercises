736a88747e844c87b2a86f8cb87a79f8fa39cd2c
Frederic Vogels <frederic.vogels@ucll.be>
2021-04-19 22:21:18 +0200

    05-modules/web/cocktail

---
+++ 05-modules/web/cocktail/cocktail.py
@@ -0,0 +1,44 @@
+from urllib.request import urlopen
+import argparse
+import json
+
+
+base_url = 'https://www.thecocktaildb.com/api/json/v1/1'
+
+def download(url):
+    with urlopen(url) as response:
+        return response.read().decode('utf-8')
+
+
+def find_by_name(name):
+    name = name.replace(' ', '%20')
+    url = f"{base_url}/search.php?s={name}"
+    data = download(url)
+    show(data)
+
+def find_random():
+    url = f"{base_url}/random.php"
+    data = download(url)
+    show(data)
+
+
+def show(data):
+    data = json.loads(data)
+    for drink in data['drinks']:
+        print(drink['strDrink'])
+        for i in range(1, 16):
+            ingredient = drink[f'strIngredient{i}']
+            if ingredient is not None:
+                amount = drink[f'strMeasure{i}'].strip()
+                print(f"{ingredient} ({amount})")
+        print()
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('name', default='', nargs='?')
+args = parser.parse_args()
+
+if args.name:
+    find_by_name(args.name)
+else:
+    find_random()
---
+++ 05-modules/web/cocktail/explanations.md
@@ -0,0 +1,23 @@
+# Explanations
+
+Use https://www.thecocktaildb.com/api.php to fetch information about cocktails.
+
+* `py cocktail.py` should show the ingredients of random cocktail.
+* `py cocktail.py "bloody mary"` should show the ingredients of the cocktail with the specified name.
+
+```bash
+$ py cocktail.py
+The Jimmy Conway
+Irish Whiskey (50 ml)
+Amaretto (50 ml)
+Cranberry Juice (4 oz)
+
+$ py cocktail.py "Bloody Mary"
+Bloody Mary
+Vodka (1 1/2 oz)
+Tomato juice (3 oz)
+Lemon juice (1 dash)
+Worcestershire sauce (1/2 tsp)
+Tabasco sauce (2-3 drops)
+Lime (1 wedge)
+```
