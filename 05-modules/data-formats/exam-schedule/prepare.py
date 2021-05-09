80e6280eea7f891327ccfdf27d16da3dcfa9c3e3
Frederic Vogels <frederic.vogels@ucll.be>
2021-05-09 17:29:03 +0200

    05-modules/data-formats/exam-schedule

--- 05-modules/data-formats/exam-schedule/prepare.py
+++ 05-modules/data-formats/exam-schedule/prepare.py
@@ -16,7 +16,17 @@ filename, _ = urlretrieve(url, reporthook=report)
 
 print('Unzipping...')
 with ZipFile(filename, 'r') as archive:
+    archive_contents = archive.namelist()
     archive.extractall('.')
+
+
+for archive_file in archive_contents:
+    print(f'Converting {archive_file}')
+    with open(archive_file, encoding='latin-1') as file:
+        contents = file.read()
+    with open(archive_file, 'w', encoding='utf-8') as file:
+        file.write(contents)
+
 
 print('Cleaning up...')
 remove(filename)
