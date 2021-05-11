749a7c3dfb243148768538c2a3bc5b6018e7e15b
Frederic Vogels <frederic.vogels@ucll.be>
2021-05-11 09:26:02 +0200

    05-modules/data-formats/json/ageplot

---
+++ 05-modules/data-formats/json/ageplot/solution.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import json
+
+
+with open('../covid.json') as file:
+    data = json.load(file)
+
+
+table = {}
+
+for entry in data:
+    age_group = entry['AGEGROUP']
+    count = entry['COUNT']
+
+    table[age_group] = table.get(age_group, 0) + count
+
+
+age_groups = sorted(table.keys())
+counts = [ table[age_group] for age_group in age_groups ]
+
+plt.rcdefaults()
+fig, ax = plt.subplots()
+ax.barh(range(len(age_groups)), counts)
+ax.set_yticklabels([0, *age_groups])
+print(table)
+plt.show()
